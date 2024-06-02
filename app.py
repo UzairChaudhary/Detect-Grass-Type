f61bcb69d6a87e7d4d555a1bcb6237884bd2418b
UzairChaudhary <uzairchaudhary.30@gmail.com>
2024-06-03 02:37:49 +0500

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 import requests
 from io import BytesIO
 from PIL import Image
-import os
+import sys
 
 app = Flask(__name__)
 
@@ -53,5 +53,5 @@ def predict():
     return jsonify({'grass_type': grass_type}),200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    print("Running app...", file=sys.stderr)
+    app.run()
